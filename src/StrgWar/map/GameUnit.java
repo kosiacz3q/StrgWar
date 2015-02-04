bede049385e77947e79b2bcaf26738c2d8b899d2
deha- <hanka.dmowska@gmail.com>
2015-02-04 15:53:02 +0100

    wstep do rysowania przesylanych jednostek

--- src/StrgWar/map/GameUnit.java
+++ src/StrgWar/map/GameUnit.java
@@ -1,11 +1,16 @@
 package StrgWar.map;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import StrgWar.core.IPlayerColorProvider;
 import StrgWar.core.IUpdateable;
+import StrgWar.gui.DrawingManager;
+import StrgWar.gui.IDrawable;
 import StrgWar.map.changeable.ChangeableNode;
 
-public class GameUnit implements IUpdateable
+public class GameUnit implements IUpdateable, IDrawable
 {
-	public GameUnit(ChangeableNode origin, ChangeableNode target,String ownerName, int count)
+	public GameUnit(ChangeableNode origin, ChangeableNode target, String ownerName, int count)
 	{
 		_roadLength = (float) origin.GetPosition().distance(target.GetPosition());
 		_traveled = 0;
@@ -26,6 +31,13 @@ public class GameUnit implements IUpdateable
 			if (IsTravelComplete())
 				EndTravel();
 		}
+	}
+	
+	@Override
+	public void Draw(GraphicsContext gc, Pane root, IPlayerColorProvider playerColorProvider, long now)
+	{
+		gc.setStroke(playerColorProvider.GetPlayerColor(Owner));
+		gc.strokeOval(_currentPosition.getX(), _currentPosition.getY(), 10, 10);
 	}
 
 	public boolean IsTravelComplete()
